d958bc22f7eafd50fea3b03e0ad217848f7decec
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-19 20:59:27 +0000

    Add event handling functions

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,30 @@
 import "./SearchBar.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-const SearchBar = () => {
+const SearchBar = ({ fetchResults }) => {
+  // component code here
+
+  SearchBar.propTypes = {
+    fetchResults: PropTypes.func.isRequired,
+  };
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    fetchResults(inputValue);
+  };
+
   return (
     <div className="search-bar">
-      <input type="text" />
-      <button>Search</button>
+      <form action="get">
+        <input value={inputValue} onChange={handleChange} type="text" />
+        <button onClick={handleClick}>Search</button>
+      </form>
     </div>
   );
 };
