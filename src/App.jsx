167d44d3892ed40f2a769beeaefb3ec5871711e5
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-19 20:58:09 +0000

    Add cocktail fetching function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import Axios from "axios";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ResultTile from "./components/ResultTille/ResultTile";
 
 function App() {
+  const fetchResults = async (query) => {
+    try {
+      const response = await Axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar fetchResults={fetchResults} />
       <ResultTile />
     </div>
   );
