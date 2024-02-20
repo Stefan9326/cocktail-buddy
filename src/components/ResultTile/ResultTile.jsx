3f53ed9238ddb854c232163e4ecc3dd6478caaeb
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-20 15:02:02 +0000

    Add toggle functionality for recipe details on tiles

--- src/components/ResultTile/ResultTile.jsx
+++ src/components/ResultTile/ResultTile.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import "./ResultTile.css";
 
 const ResultTile = ({ result }) => {
+  const [recipeDisplayed, setRecipeDisplayed] = useState(false);
+
+  const toggleRecipeDisplay = () => {
+    setRecipeDisplayed(!recipeDisplayed);
+  };
+
   return (
     <div className="result-tile">
       {result.strDrink}
       <img src={result.strDrinkThumb} alt={`Photo of ${result.strDrink}`} />
+      <button onClick={toggleRecipeDisplay}>Show recipe</button>
+      {recipeDisplayed && <p>RECIPE</p>}
     </div>
   );
 };
