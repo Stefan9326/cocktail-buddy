9545f5066618798564cbcad2665e6594ad033e15
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-20 00:36:41 +0000

    Add images to tiles

--- src/components/ResultTile/ResultTile.jsx
+++ src/components/ResultTile/ResultTile.jsx
@@ -2,7 +2,12 @@ import PropTypes from "prop-types";
 import "./ResultTile.css";
 
 const ResultTile = ({ result }) => {
-  return <div className="result-tile">{result.strDrink}</div>;
+  return (
+    <div className="result-tile">
+      {result.strDrink}
+      <img src={result.strDrinkThumb} alt={`Photo of ${result.strDrink}`} />
+    </div>
+  );
 };
 
 ResultTile.propTypes = {
