4f0ca05009b6aa85a45ef5620f9e3b38aca7175a
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-22 01:16:44 +0000

    Show cocktail instructions when "Show recipe" button is clicked
    - Use Axios to fetch the cocktail details from the API
    - Display the instructions when the "Show recipe" button is clicked

--- src/components/ResultTile/ResultTile.jsx
+++ src/components/ResultTile/ResultTile.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
 import PropTypes from "prop-types";
-import { useState } from "react";
 import "./ResultTile.css";
 
 const ResultTile = ({ result }) => {
+  const [cocktailInfo, setCocktailInfo] = useState([]);
   const [recipeDisplayed, setRecipeDisplayed] = useState(false);
+
+  useEffect(() => {
+    const fetchCocktailById = async () => {
+      try {
+        const response = await Axios.get(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${result.idDrink}`
+        );
+        setCocktailInfo(response.data.drinks[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCocktailById();
+  }, [result.idDrink]);
 
   const toggleRecipeDisplay = () => {
     setRecipeDisplayed(!recipeDisplayed);
@@ -15,7 +31,11 @@ const ResultTile = ({ result }) => {
       <div className="right">
         <img src={result.strDrinkThumb} alt={`Photo of ${result.strDrink}`} />
         <button onClick={toggleRecipeDisplay}>Show recipe</button>
-        {recipeDisplayed && <p>RECIPE</p>}
+        {recipeDisplayed && (
+          <div>
+            <p>{cocktailInfo["strInstructions"]}</p>
+          </div>
+        )}
       </div>
     </div>
   );
