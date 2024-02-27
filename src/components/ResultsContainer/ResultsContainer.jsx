379fe63aa7372f3ebc5e35c598508fe18aec7ae0
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-27 15:00:09 +0000

    Show message when no exact matches are found

--- src/components/ResultsContainer/ResultsContainer.jsx
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -38,9 +38,11 @@ const ResultsContainer = ({ results, resultsLimit }) => {
         });
     }
   }
+  const noExactResults = !commonDrinks.length;
 
   return (
     <div className="results-container">
+      {noExactResults && <p>No exact results found</p>}
       {results.length ? (
         drinksToDisplay.slice(0, resultsLimit).map((result) => {
           return <ResultTile key={result.idDrink} result={result} />;
