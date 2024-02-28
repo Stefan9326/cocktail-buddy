186812f26f2fa825d70c89d84c6a41c089251ee4
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-28 23:41:48 +0000

    Initialize router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
+const router = createBrowserRouter(<Route path="/" element="" />);
 
 const queryClient = new QueryClient();
 
@@ -13,5 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
