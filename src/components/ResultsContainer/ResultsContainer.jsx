461103d6e7613d01683e7cead0a943d74ed92313
Stefan Shambrook <stefshambrook@gmail.com>
2024-02-28 19:39:59 +0000

    Move results processing logic to a custom hook

--- src/components/ResultsContainer/ResultsContainer.jsx
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -1,38 +1,10 @@
+import { useDrinksToDisplay } from "../../hooks/useDrinksToDisplay";
 import PropTypes from "prop-types";
 import ResultTile from "../ResultTile/ResultTile";
 import "./ResultsContainer.css";
 
 const ResultsContainer = ({ results, resultsLimit, dropdowns }) => {
-  const drinksCount = {};
-  let commonDrinks = [];
-  let drinksToDisplay = results[0];
-  let noExactResults = false;
-
-  if (results.length > 1) {
-    results.forEach((result) => {
-      result.forEach((drink) => {
-        if (drinksCount[drink.strDrink]) {
-          drinksCount[drink.strDrink]++;
-        } else {
-          drinksCount[drink.strDrink] = 1;
-        }
-      });
-    });
-    // Obtain an array of drinks' names that are common to all results
-    commonDrinks = Object.keys(drinksCount).filter((drink) => drinksCount[drink] === results.length);
-    console.log(results[0].filter((drink) => commonDrinks.includes(drink.strDrink)));
-    // Return an array of drinks objects that are common to all results
-    if (commonDrinks.length) {
-      drinksToDisplay = results[0].filter((drink) => commonDrinks.includes(drink.strDrink));
-    } else {
-      noExactResults = true;
-      drinksToDisplay = Object.entries(drinksCount)
-        .sort((a, b) => b[1] - a[1])
-        .map((drinkCount) => {
-          return results.flat().find((drink) => drink.strDrink === drinkCount[0]);
-        });
-    }
-  }
+  const { drinksToDisplay, noExactResults } = useDrinksToDisplay(results);
 
   return (
     <div className="results-container">
