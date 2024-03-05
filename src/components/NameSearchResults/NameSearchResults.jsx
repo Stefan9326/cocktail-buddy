61685146ad6492b842e74ec0076589ada6bff7a8
Stefan Shambrook <stefshambrook@gmail.com>
2024-03-05 15:37:28 +0000

    Fetch results and display once query is resolved

--- src/components/NameSearchResults/NameSearchResults.jsx
+++ src/components/NameSearchResults/NameSearchResults.jsx
@@ -1,7 +1,26 @@
 import { PropTypes } from "prop-types";
+import useFetchCocktailsByName from "../../hooks/useFetchCocktailsByName";
 
 const NameSearchResults = ({ userInput }) => {
-  return <p>{userInput}</p>;
+  const { isSuccess, results } = useFetchCocktailsByName(userInput);
+
+  if (!isSuccess) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <div className="results-container">
+        {results.map((result) => {
+          return (
+            // This will be ResultTile once I have refactored to allow it to be reusable in different contexts
+            <div key={result.idDrink}>
+              <h3>{result.strDrink}</h3>
+              <img src={result.strDrinkThumb} alt={`Photo of ${result.strDrink}`} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 NameSearchResults.propTypes = {
