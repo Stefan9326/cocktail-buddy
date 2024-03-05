fc381f02b30f597be876581b0fcb212baf2fa853
Stefan Shambrook <stefshambrook@gmail.com>
2024-03-05 22:20:54 +0000

    Fix component names

---
+++ src/components/IngredientSearchResultTile/IngredientSearchResultTile.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { memo } from "react";
+import useFetchCocktailInfo from "../../hooks/useFetchCocktailInfo";
+import PropTypes from "prop-types";
+import "./IngredientSearchResultTile.css";
+
+export const IngredientSearchResultTile = memo(function ResultTile({ result, noExactResults, dropdowns }) {
+  const [recipeDisplayed, setRecipeDisplayed] = useState(false);
+  const { cocktailInfo, isSuccess, ingredients, matchedIngredients } = useFetchCocktailInfo(result, dropdowns);
+
+  const toggleRecipeDisplay = () => {
+    setRecipeDisplayed(!recipeDisplayed);
+  };
+
+  return (
+    <div className="result-tile">
+      <div className="initial-info">
+        {result.strDrink}
+        {noExactResults && (
+          <p>
+            Matched ingredients:{" "}
+            <em>
+              <strong>{matchedIngredients.join(", ")}</strong>
+            </em>
+          </p>
+        )}
+        <div className="right">
+          <img src={result.strDrinkThumb} alt={`Photo of ${result.strDrink}`} />
+          <button onClick={toggleRecipeDisplay}>Show recipe</button>
+        </div>
+      </div>
+      {recipeDisplayed && isSuccess && (
+        <div>
+          <ul>
+            {ingredients.map((ingredient, index) => {
+              return (
+                <li key={ingredient + index}>
+                  {ingredient}
+                  {cocktailInfo[`strMeasure${index + 1}`]}
+                </li>
+              );
+            })}
+          </ul>
+          <p>{cocktailInfo["strInstructions"]}</p>
+        </div>
+      )}
+    </div>
+  );
+});
+
+IngredientSearchResultTile.propTypes = {
+  result: PropTypes.object.isRequired,
+  noExactResults: PropTypes.bool.isRequired,
+  dropdowns: PropTypes.array.isRequired,
+};
